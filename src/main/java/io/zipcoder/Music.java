bb147223dbb7382e37bdf2c757606e4010d6eed2
Henry <cataniahf@gmail.com>
2019-06-01 16:56:13 -0400

    Finished lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,31 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int count = 0;
+        int count2 = 1;
+
+        for (int i = startIndex; i < playList.length; i++) {
+            if (playList[i] == selection) {
+                break;
+            }
+            count++;
+        }
+        for (int j = startIndex; j < playList.length; j--) {
+            if (j == 0) {
+                j = playList.length - 1;
+            }
+            if (playList[j] == selection) {
+                break;
+            }
+            count2++;
+        }
+
+
+        return Math.min(count, count2);
     }
 }
